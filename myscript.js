ef5b7dea902b3ef68ab6e1f16eff100e63221d18
Elton Bautista <eltonbestaha@gmail.com>
2022-01-05 14:32:14 -0700

    Probably going to need to refactor logic on squares

--- myscript.js
+++ myscript.js
@@ -1,42 +1,35 @@
 const container = document.querySelector('#container');
 
 
-for (let i = 0; i < 16; i++) {
-    const squareOne = document.createElement('div');
-    squareOne.classList.add('square');
-    squareOne.style.backgroundColor = 'yellow';
-    squareOne.style.minWidth = '45px';
-    squareOne.style.minHeight = '45px';
-    squareOne.style.border = '2.5px solid black'
-    squareOne.style.borderRight = '2.5px solid black'
-    squareOne.style.borderTop = '5px solid black'
-    container.appendChild(squareOne);
+
+const drawSquares = function() {
+for (let i = 0; i < 16*16; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.display = 'flex';
+        square.style.backgroundColor = 'red';
+        square.style.minWidth = '45px';
+        square.style.minHeight = '45px';
+        square.style.border = '2.5px solid black'
+        square.style.borderRight = '2.5px solid black';
+        square.style.borderLeft = '2.5px solid black';
+        container.appendChild(square);
+if (i < 16) {
+    square.style.borderTop = '5px solid black';
+} else if (i >= 240) {
+    square.style.borderBottom = '5px solid black';
+} 
+if (i % 16 === 0) {
+    square.style.borderLeft = '5px solid black';
+} 
+   if(i % 16 === 15) {
+    square.style.borderRight = '5px solid black';
 }
-
-for (let i = 0; i < 224; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.backgroundColor = 'red';
-    square.style.minWidth = '45px';
-    square.style.minHeight = '45px';
-    square.style.border = '2.5px solid black'
-    square.style.borderRight = '2.5px solid black';
-    square.style.borderLeft = '2.5px solid black';
-    container.appendChild(square);
-
+square.addEventListener('click', (e) => {
+    square.style.backgroundColor = 'yellow';
+    square.style.filter = 'blur(4px)';
+})
 }
-
-
-for (let i = 0; i < 16; i++) {                                                                                                                                                       
-    const squareLast = document.createElement('div');
-    squareLast.classList.add('square');
-    squareLast.style.backgroundColor = 'yellow';
-    squareLast.style.minWidth = '45px';
-    squareLast.style.minHeight = '45px';
-    squareLast.style.border = '2.5px solid black'
-    squareLast.style.borderLeft = '2.5px solid black'
-    squareLast.style.borderBottom = '5px solid black'
-    container.appendChild(squareLast);
 }
-
+drawSquares();
 
