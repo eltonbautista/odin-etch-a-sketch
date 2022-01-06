11799527fce25d5c8bd24341ba953c25627c7a92
Elton Bautista <eltonbestaha@gmail.com>
2022-01-05 20:08:11 -0700

    Worked on the logic for choosing grid size. Need to work on how to properly scale squares.

--- myscript.js
+++ myscript.js
@@ -1,19 +1,6 @@
 const container = document.querySelector('#container');
+/* 
 
-
-
-const drawSquares = function() {
-for (let i = 0; i < 16*16; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.display = 'flex';
-        square.style.backgroundColor = 'red';
-        square.style.minWidth = '45px';
-        square.style.minHeight = '45px';
-        square.style.border = '2.5px solid black'
-        square.style.borderRight = '2.5px solid black';
-        square.style.borderLeft = '2.5px solid black';
-        container.appendChild(square);
 if (i < 16) {
     square.style.borderTop = '5px solid black';
 } else if (i >= 240) {
@@ -25,11 +12,45 @@ if (i % 16 === 0) {
    if(i % 16 === 15) {
     square.style.borderRight = '5px solid black';
 }
+        
+        */
+
+const makeGrid = function(size) {
+for (let i = 0; i < size; i++) {
+const squareContainer = document.createElement('div');
+squareContainer.classList.add('squareContainer');
+squareContainer.style.display = 'flex';
+squareContainer.style.justifyContent = 'center';
+squareContainer.style.maxWidth = '850px';
+squareContainer.style.width = '850px';
+squareContainer.style.maxHeight = '850px';
+squareContainer.style.minHeight = 'auto';
+squareContainer.style.backgroundColor = 'white'
+//squareContainer.style.border = '5px solid yellow'
+squareContainer.style.margin = '0';
+container.appendChild(squareContainer);
+
+for (let i = 0; i < size; i++) {
+const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.backgroundColor = 'red';
+const width = square.style.maxWidth = '15px';
+        square.style.width = '100%';
+        square.style.flex = '1 0 0';
+        square.style.height = width;
+        square.style.border = '1px solid black'
+        squareContainer.appendChild(square);
+
 square.addEventListener('click', (e) => {
     square.style.backgroundColor = 'yellow';
-    square.style.filter = 'blur(4px)';
-})
+    square.style.filter = 'blur(2px)';
+})         
 }
 }
-drawSquares();
+}
 
+function numberOfGrid() {
+   let input = prompt('How big would you like it?')
+   makeGrid(input);
+}
+numberOfGrid();
