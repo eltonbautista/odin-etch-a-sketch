87e804f60c85b1b49da6b4efc1e7312231eb300e
Elton Bautista <eltonbestaha@gmail.com>
2022-01-06 20:11:30 -0700

    I think I've ironed out most of the necessary logic. Need to add better styling, as well as figuring out how to only use cells without extra divs

--- myscript.js
+++ myscript.js
@@ -1,56 +1,135 @@
 const container = document.querySelector('#container');
-/* 
-
-if (i < 16) {
-    square.style.borderTop = '5px solid black';
-} else if (i >= 240) {
-    square.style.borderBottom = '5px solid black';
-} 
-if (i % 16 === 0) {
-    square.style.borderLeft = '5px solid black';
-} 
-   if(i % 16 === 15) {
-    square.style.borderRight = '5px solid black';
-}
-        
-        */
+const restartBtn = document.querySelector('#restart-button')
+const buttonContainer = document.querySelector('#button-container');
 
 const makeGrid = function(size) {
 for (let i = 0; i < size; i++) {
 const squareContainer = document.createElement('div');
 squareContainer.classList.add('squareContainer');
 squareContainer.style.display = 'flex';
+squareContainer.style.flex = '1 0 auto'
 squareContainer.style.justifyContent = 'center';
-squareContainer.style.maxWidth = '850px';
-squareContainer.style.width = '850px';
-squareContainer.style.maxHeight = '850px';
-squareContainer.style.minHeight = 'auto';
+squareContainer.style.width = '100%';
 squareContainer.style.backgroundColor = 'white'
-//squareContainer.style.border = '5px solid yellow'
-squareContainer.style.margin = '0';
-container.appendChild(squareContainer);
+buttonContainer.appendChild(squareContainer);
 
 for (let i = 0; i < size; i++) {
 const square = document.createElement('div');
         square.classList.add('square');
+        square.style.display = 'flex';
         square.style.backgroundColor = 'red';
-const width = square.style.maxWidth = '15px';
         square.style.width = '100%';
-        square.style.flex = '1 0 0';
-        square.style.height = width;
-        square.style.border = '1px solid black'
+        square.style.height = '100%';
+        square.style.outline = '1px solid black'
         squareContainer.appendChild(square);
-
-square.addEventListener('click', (e) => {
+const yellowEffect = function(){
     square.style.backgroundColor = 'yellow';
-    square.style.filter = 'blur(2px)';
-})         
+    square.style.filter = 'blur(6px)';
+}
+const greyEffect = function(){
+    square.style.backgroundColor = 'grey';
+    square.style.filter = 'blur(6px)';
+}
+const whiteEffect = function() {
+    square.style.backgroundColor = 'white';
+    square.style.filter = 'blur(6px)';
+}
+const blackEffect = function() {
+    square.style.backgroundColor = 'black';
+    square.style.filter = 'blur(6px)'
+}
+const yellowButton = function() {
+square.addEventListener('mouseenter', yellowEffect);
+}
+const greyButton = function() {
+square.addEventListener('mouseenter', greyEffect);
+}
+const whiteButton = function() {
+square.addEventListener('mouseenter', whiteEffect);
+}
+const blackButton = function() {
+square.addEventListener('mouseenter', blackEffect);
+}
+const stopYellow = function () {
+square.removeEventListener('mouseenter', yellowEffect);
+}
+const stopGrey = function(){
+square.removeEventListener('mouseenter', greyEffect);
+}
+const stopWhite = function () {
+square.removeEventListener('mouseenter', whiteEffect);
+}
+const stopBlack = function() {
+square.removeEventListener('mouseenter', blackEffect);
+}
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'e'){
+        stopYellow();
+        stopGrey();
+        stopWhite();
+        stopBlack();
+};
+});
+    
+document.addEventListener('keydown', e => {
+    if (e.key === 's'){
+        yellowButton();
+        stopGrey();
+        stopWhite();
+        stopBlack();
+    }
+});
+document.addEventListener('keydown', e => {
+    if(e.key === 'd') {
+        greyButton();
+        stopWhite();
+        stopBlack();
+        stopYellow();
+    }
+});
+document.addEventListener('keydown', e => {
+    if(e.key === 'w') {
+        whiteButton();
+        stopYellow();
+        stopGrey();
+        stopBlack();
+    }
+});
+document.addEventListener('keydown', e => {
+    if(e.key === 'a') {
+        blackButton();
+        stopYellow();
+        stopGrey();
+        stopWhite();
+    }
+});
 }
 }
+}
+
+makeGrid(16);
+
+const clearDiv = function() {
+    document.querySelector('#button-container').innerText = '';
 }
 
 function numberOfGrid() {
-   let input = prompt('How big would you like it?')
-   makeGrid(input);
+    let input = prompt('How big would you like it? No bigger than 100 please! ')
+    if (input <= 100 && input > 0){
+        clearDiv();
+    makeGrid(input);
+    } else if (input > 100) {
+        alert("That's TOO BIG!");
+    } else if (input === null || input === ''){
+    }
+ }
+
+const reload = function() {
+    window.location.reload();
 }
-numberOfGrid();
+//restartBtn.addEventListener('click', clearDiv);
+restartBtn.addEventListener('click', numberOfGrid);
+//restartBtn.addEventListener('click', reload);
+
+
